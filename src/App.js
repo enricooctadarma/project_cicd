12ad8957bbb07e32905063aa217c364f29fe8f7a
enricooctadarma <enricohulu18@gmail.com>
2025-06-03 10:05:51 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/photos?_limit=5')
+      .then(response => response.json())
+      .then(data => setPhotos(data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Galeri Foto</h1>
+      <ul>
+        {photos.map(photo => (
+          <li key={photo.id}>
+            <img src={photo.thumbnailUrl} alt={photo.title} width="150" />
+            <p>{photo.title}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
